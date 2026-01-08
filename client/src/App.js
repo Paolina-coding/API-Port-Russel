f1529fac737483ce2f0e74fd828fa9eb8d563123
PaolinaA <paolina.info@gmail.com>
2026-01-08 02:18:37 +0100

    Frontend et lien avec le serveur pour les reservations

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import TableauDeBord from "./components/pages/tableau_de_bord";
 import Catways from "./components/pages/catways";
 import Reservations from "./components/pages/reservations";
 import GetCatway from "./components/getCatway.jsx";
+import ReservationsCatway from "./components/pages/reservationsCatway";
+import GetReservation from "./components/getReservation";
 // import Utilisateurs from "./components/pages/utilisateurs";
 
 import './App.css';
@@ -19,7 +21,8 @@ function App() {
         <Route path="/catways" element={<Catways />} />
         <Route path="/reservations" element={<Reservations />} />
         <Route path="/catways/:id" element={<GetCatway />} />
-        
+        <Route path="catways/:catwayNumber/reservations/" element={<ReservationsCatway />} />
+        <Route path="/catways/:catwayNumber/reservations/:idReservation" element={<GetReservation />} />
         {/*<Route path="/:catwayNumber/reservations/add" element={<AddReservation />} />
         <Route path="/catways/:catwayNumber//List" element={<ReservationList />} />
         <Route path="/catways/:catwayNumber/reservations/:idReservation/get" element={<GetReservation />} />
